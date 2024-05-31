94256edf381faa8ee0b6100a00d746d33738d351
Byasar3 <bernayasar@hotmail.co.uk>
2024-05-31 11:49:40 +0100

    initial commit

---
+++ src/main/java/com/selfhelpstation/selfhelpstation/SelfHelpStationApplication.java
@@ -0,0 +1,13 @@
+package com.selfhelpstation.selfhelpstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfHelpStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfHelpStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfhelpstation/selfhelpstation/SelfHelpStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfhelpstation.selfhelpstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfHelpStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
